21217082bddbaeec90637ae695c8e99a1cea032c
LS4Tonio <toniolesniper@gmail.com>
2016-03-17 13:42:13 +0100

    Extended CarStatus

---
+++ FirstSolution/ITI2016.Dev.Tests/CarStatusTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ITI2016.Dev.Tests
+{
+    [TestFixture]
+    public class CarStatusTests
+    {
+        [Test]
+        public void CarStatus_basic_flags()
+        {
+            var s = CarStatus.IsRunning | CarStatus.IsBrakePedal;
+            Assert.That((s & CarStatus.IsRunning) != 0, "The car is running.");
+            Assert.That((s & CarStatus.IsClutchPedal) == 0, "The clutch pedal is NOT pressed.");
+
+            s = s & ~CarStatus.IsRunning;
+            Assert.That((s & CarStatus.IsRunning) == 0, "The car is stopped.");
+        }
+
+        [Test]
+        public void CarStatus_gear_management()
+        {
+            // Gear 1
+            var s = CarStatus.IsRunning | (CarStatus)(1 << 5);
+            Assert.That(s.GetGear(), Is.EqualTo(CarGear.First));
+            Assert.That((Int32)s.GetGear(), Is.EqualTo(1));
+
+            // Speed 3
+            s = s.SetGear(CarGear.Third);
+            Assert.That(s.GetGear(), Is.EqualTo(CarGear.Third));
+
+            // Speed 5
+            s = s.SetGear(5);
+            Assert.That(s.GetGear(), Is.EqualTo(CarGear.Fifth));
+        }
+
+        [Test]
+        public void CarStatus_status_check()
+        {
+            var s = CarStatus.IsRunning;
+            Assert.That(s.HasStatus(CarStatus.IsRunning), Is.True, "running");
+
+            s = s | CarStatus.IsBrakePedal;
+            Assert.That(s.HasStatus(CarStatus.IsBrakePedal), Is.True, "brake pedal");
+
+            s = s.ToggleStatus(CarStatus.IsBrakePedal);
+            Assert.That(s.HasStatus(CarStatus.IsBrakePedal), Is.False, "brake pedal");
+        }
+    }
+}
--- FirstSolution/ITI2016.Dev/CarStatus.cs
+++ FirstSolution/ITI2016.Dev/CarStatus.cs
@@ -1,23 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ITI2016.Dev
 {
-    /*
-    A Car can be in multiple states:
-    - The engine is running or not.
-    - There may be a speed engaged (typically 5 speeds)
-    - The clutch pedal can be pressed or not.
-    - The gas pedal can be pressed or not.
-    - The brake pedal can be pressed or not.
-    - The hand brake can be set or not.
+    /// <summary>
+    /// A Car can be in multiple states:
+    /// - The engine is running or not.
+    /// - There may be a speed engaged(typically 5 speeds)
+    /// - The clutch pedal can be pressed or not.
+    /// - The gas pedal can be pressed or not.
+    /// - The brake pedal can be pressed or not.
+    /// - The hand brake can be set or not.
+    /// </summary>
+    public enum CarStatus : byte
+    {
+        IsRunning = 1 << 0,
+        IsClutchPedal = 1 << 1,
+        IsGasPedal = 1 << 2,
+        IsBrakePedal = 1 << 3,
+        IsHandBrake = 1 << 4,
+        GearMask = 7 << 5
+    }
+    
+    public enum CarGear
+    {
+        None = 0,
+        First = 1,
+        Second = 2,
+        Third = 3,
+        Fourth = 4,
+        Fifth = 5,
+        Backward = 6
+    }
+    
+    public static class CarStatusExtension
+    {
+        public static CarGear GetGear(this CarStatus status)
+        {
+            return (CarGear)((Int32)status >> 5);
+        }
 
-    How do you (efficiently) encode this state?
-    */
+        public static CarStatus SetGear(this CarStatus status, CarGear gear)
+        {
+            return SetGear(status, (Int32)gear);
+        }
 
+        public static CarStatus SetGear(this CarStatus status, Int32 gear)
+        {
+            if (gear < 0 || gear > 6) throw new ArgumentOutOfRangeException();
+            return (status & ~CarStatus.GearMask) | (status | (CarStatus)(gear << 5));
+        }
 
+        public static CarStatus ToggleStatus(this CarStatus status, CarStatus statusToChange)
+        {
+            return status ^ statusToChange;
+        }
 
+        public static Boolean HasStatus(this CarStatus status, CarStatus s)
+        {
+            return (status & s) != 0;
+        }
+    }
 }
