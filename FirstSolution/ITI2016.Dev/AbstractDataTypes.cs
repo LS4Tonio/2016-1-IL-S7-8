211214b9033db43783a06d9f703d711524402414
LS4Tonio <toniolesniper@gmail.com>
2016-03-18 11:08:21 +0100

    Interfaces & implémentation de List<T>

---
+++ FirstSolution/ITI2016.Dev/AbstractDataTypes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ITI2016.Dev
+{
+    public interface IEnumerator<T>
+    {
+        T Current { get; }
+
+        Boolean MoveNext();
+    }
+
+    public interface IEnumeratorJava<T>
+    {
+        Boolean HasNext();
+        T GetNext();
+    }
+
+    public interface IEnumerable<T>
+    {
+        IEnumerator<T> GetEnumerator();
+    }
+
+    public interface IReadOnlyCollection<T> : IEnumerable<T>
+    {
+        Int32 Count { get; }
+    }
+
+    public interface IReadOnlyList<T> : IReadOnlyCollection<T>
+    {
+        T this[Int32 index] { get; }
+    }
+
+    public interface IList<T> : IReadOnlyList<T>
+    {
+        new T this[Int32 index] { get; set; }
+
+        void Add(T item);
+        void InsertAt(Int32 index, T item);
+        void RemoveAt(Int32 index);
+        void Clear();
+    }
+
+    public interface ISet<T> : IReadOnlyCollection<T>
+    {
+        void Add(T item);
+        void Contains(T item);
+        void Remove(T item);
+    }
+}
